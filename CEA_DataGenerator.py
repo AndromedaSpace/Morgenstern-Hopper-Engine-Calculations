acc3b0adbde8d175a83f50884ddc3c01cba8f1b5
Christos Margaritis <margaritis.christos@outlook.com>
2020-05-02 22:12:19 +0300

    Multiprocessing for Data Generator and updated gitignore

--- CEA_DataGenerator.py
+++ CEA_DataGenerator.py
@@ -1,6 +1,7 @@
 from rocketcea.cea_obj import add_new_fuel, add_new_propellant
 from rocketcea.cea_obj_w_units import CEA_Obj
 import time
+from multiprocessing import Process, Queue
 
 
 card_str = """
@@ -10,16 +11,14 @@ card_str = """
 add_new_fuel( 'paraffin', card_str )
 
 
-
-
 Pe = 101325 #Pa
 
 EPSmin = 1
-EPSmax = 3
+EPSmax = 1.8 #3
 dEPS = 0.05
 
 Pmin = 2 * 10**5 # Pa
-Pmax = 30 * 10**5 # Pa
+Pmax = 2.2 * 10**5 # Pa 30 
 dP = 0.05 * 10**5 # Pa
 
 OFmin = 2
@@ -29,37 +28,80 @@ dOF = 0.05
 ox = "N2O"
 fuel = "paraffin"
 
-outFile = open("cea_results.txt", 'w')
-C = CEA_Obj(oxName=ox, fuelName=fuel, pressure_units='Pa')
+filename = "cea_results.txt"
 
-cP = Pmin
+nProcesses = 3
 
-totalPSteps = (EPSmax-EPSmin)/dP
-cEPS = EPSmin
-last_time = time.time()
-while cEPS < EPSmax:
-    cP = Pmin
-    while cP <= Pmax:
-        print(cP)
-        cOF = OFmin
-        while cOF <= OFmax:
-            outFile.write(str(cEPS) + ' ')
-            outFile.write(str(cP) + ' ')
-            outFile.write(str(cOF) + ' ')
 
-            Ivac,Cstr,Tc = C.get_IvacCstrTc(Pc=cP, MR=cOF, eps=cEPS)
-            outFile.write(str(Ivac) + ' ')
-            outFile.write(str(Cstr) + ' ')
-            outFile.write(str(Tc) + ' ')
 
-            Cf = C.get_PambCf(Pamb=Pe, Pc=cP, MR=cOF, eps=cEPS)
-            outFile.write(str(Cf[1]) + '\n')
+def worker(q,EPSmin, EPSmax , dEPS , Pmin , Pmax, dP, OFmin , OFmax, dOF, printProgress):
+    C = CEA_Obj(oxName=ox, fuelName=fuel, pressure_units='Pa')
+    cEPS = EPSmin
+    if  (printProgress):
+        last_time = time.time()
+    while cEPS <= EPSmax:
+        cP = Pmin
+        while cP <= Pmax:
+            cOF = OFmin
+            while cOF <= OFmax:
+                Ivac,Cstr,Tc = C.get_IvacCstrTc(Pc=cP, MR=cOF, eps=cEPS)
 
-            cOF += dOF
-        cP += dP
-    cEPS += dEPS
-    cTime = time.time()
-    print("Progress", str((cEPS-EPSmin)/(EPSmax-EPSmin) *100),'%. ETA: ' , str((cTime-last_time) * (EPSmax-cEPS)/dEPS), 's')
-    last_time = cTime
+                Cf = C.get_PambCf(Pamb=Pe, Pc=cP, MR=cOF, eps=cEPS)
 
-outFile.close()
+                q.put([cEPS,cP,cOF,Ivac,Cstr,Tc,Cf[1],Cf[2]])
+
+                cOF += dOF
+            cP += dP
+        cEPS += dEPS
+        if  (printProgress):
+            cTime = time.time()
+            print("Progress: %.2f%% ETA: %.2fs" % ((cEPS-EPSmin)/(EPSmax-EPSmin) *100 , (cTime-last_time) * (EPSmax-cEPS)/dEPS) )
+            last_time = cTime
+    return
+
+if __name__ == '__main__':
+    EPS_step = (EPSmax-EPSmin)/nProcesses
+    jobs = []
+    q = Queue()
+    print("Generating Processes")
+    for i in range(nProcesses):
+        pEPSmin = EPSmin+EPS_step*i
+        pEPSmax = EPSmin+EPS_step*(i+1)
+        #sprint("EPSmin:" , pEPSmin, "EPSmax", pEPSmax)
+        if i == nProcesses - 1:
+            printProgress = True
+        else:
+            printProgress = False
+        
+        p = Process(target=worker, args=(q,pEPSmin,pEPSmax,dEPS,Pmin,Pmax,dP,OFmin,OFmax,dOF,printProgress))
+        jobs.append(p)
+        p.start()
+    print("Processes Started.\nGenerating Data.")
+    data = [[],[],[],[],[],[],[],[]]
+    for job in jobs:
+        while job.is_alive():
+            while not q.empty():
+                entry = q.get()
+                for i in range(len(entry)):
+                    data[i].append(entry[i])
+    print("Data Collected.")
+
+    while not q.empty():
+        data.append(q.get())
+    
+    indices = list(range(len(data[0])))
+    indices.sort(key=data[0].__getitem__)
+
+    for i , sublist in enumerate(data):
+        data[i] = [sublist[j] for j in indices]
+    print("Writing data.")
+    outFile = open(filename, 'w')
+    outFile.write("#Format: eps Pc OF Ivac Cstr Tc Cf SeparetionState")
+    for i in range(len(data[0])):
+        outStr = ""
+        for j in range(len(data)):
+            outStr += str(data[j][i]) + ' '
+        outStr = outStr[:-1] + '\n'
+        outFile.write(outStr)
+    outFile.close()
+    print("Data written. Exiting.")
