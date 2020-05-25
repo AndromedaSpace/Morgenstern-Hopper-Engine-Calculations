0b6c07ab2d8b1bc7cad2ea7986413c3f47d8f866
Christos Margaritis <margaritis.christos@outlook.com>
2020-05-26 02:38:28 +0300

    Basic Functionality

---
+++ optimiser.py
@@ -0,0 +1,48 @@
+import json
+from engineSimulator import engineSimulator
+
+class engineOptimiser:
+    setupData = None
+    engine = None
+
+    def __init__ (self):
+        self.setupData = self.readData()
+        self.engine = self.initEngine(self.setupData['engine'])
+
+
+    def readData(self, filename = "optimiser.conf"):
+        inFile = open(filename , 'r')
+        jsonString = inFile.read()
+        inFile.close()
+        return json.loads(jsonString)
+
+    def initEngine(self , data):
+        engine = engineSimulator(
+            y0 = data['y0'],
+            accentDecentAccel = data['accentDecentAccel'],
+            Tb = data['Tb'],
+            Ti = data['Ti'],
+            m0 = data['m0'],
+            A = data['A'],
+            oxName = data['oxName'],
+            fuelName = data['fuelName'],
+            fuelRho = data['fuelRho'],
+            a = data['a'],
+            n = data['n'],
+            expsHalf = data['expsHalf'],
+            Inef = data['Inef']
+        )
+        
+        engine.setMechanicalLimits(
+            Pmax = data['Pmax'],
+            Tmax = data['Tmax'],
+            Mmax = data['Mmax'],
+            PtT = data['PtT']
+        )
+
+        return engine
+
+
+
+if __name__ == "__main__":
+    optimiser = engineOptimiser()
